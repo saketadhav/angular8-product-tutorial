fc443026275fabb503f860fc4b9f745166d969f8
saketadhav <adhav.saket@gmail.com>
2019-08-10 16:11:19 +0530

    add and get opertaions with mongodb integration

---
+++ angular8-product-tutorial-ui/src/app/product-get/product-get.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import Product from '../models/Product';
+import { ProductsService } from '../services/products.service';
+
+@Component({
+  selector: 'app-product-get',
+  templateUrl: './product-get.component.html',
+  styleUrls: ['./product-get.component.css']
+})
+export class ProductGetComponent implements OnInit {
+
+  products: Product[];
+  constructor(private ps: ProductsService) { }
+
+  ngOnInit() {
+    this.ps
+      .getProducts()
+      .subscribe((data: Product[]) => {
+        this.products = data;
+    });
+  }
+
+}
